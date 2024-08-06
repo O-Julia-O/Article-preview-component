ef4fad06cf6d2c42ca7a2772cd9ea78eb2f8aafa
O-Julia-O <julia.qq.onix@gmail.com>
2024-08-06 16:25:21 +0300

    fixed button color while opening the share bbutton

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const socialPlaceholder = document.getElementById('social-media-placeholder');
 const buttonShare = document.querySelector('.content-button');
+const contentButton = document.querySelector('.content-button');
 
 
 buttonShare.addEventListener('click', function() {
     /* socialPlaceholder.classList.remove('display-none'); */
     socialPlaceholder.classList.toggle('social-media-placeholder');
+    contentButton.classList.toggle('content-button-open');
+    buttonShare.classList.toggle('shape-button-open');
 });
